a8ea0f610186fea0ed757177ff0ed7994d9605a2
PaperStrange <Dr_in_Dream@163.com>
2019-03-11 20:28:33 +0800

    FIX: fix style out of rule

--- utils/pipeline/pipeline_ETL_template.py
+++ utils/pipeline/pipeline_ETL_template.py
@@ -1,11 +1,14 @@
 # Note 2019/3/2: use sklearn pipline foolwed the instruction from Udacity
-from sklearn.pipeline import Pipeline
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
 
 X_train, y_train, X_test, y_test = [], [], [], []
 
 pipline = Pipeline([
     ("name", "a transformation"),
-    # ...
     ("name", "a classifier or a transfomation")
 ])
 
